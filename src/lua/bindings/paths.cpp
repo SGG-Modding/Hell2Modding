aa535d21d8f0d9f90b91a28d150a5725468da739
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-04-29 15:07:00 +0200

    feat(lua): add h2m.paths.Content and h2m.paths.Ship

--- src/lua/bindings/paths.cpp
+++ src/lua/bindings/paths.cpp
@@ -33,10 +33,43 @@ namespace lua::paths
 		return (char*)big::g_lua_manager->get_plugins_data_folder().get_path().u8string().c_str();
 	}
 
+	static std::filesystem::path get_game_executable_folder()
+	{
+		char module_file_path[MAX_PATH];
+		const auto path_size              = GetModuleFileNameA(nullptr, module_file_path, MAX_PATH);
+		std::filesystem::path root_folder = std::string(module_file_path, path_size);
+		root_folder                       = root_folder.parent_path();
+
+		return root_folder;
+	}
+
+	// Lua API: Function
+	// Table: h2m.paths
+	// Name: Content
+	// Returns: string: Returns the GameFolder/Content folder path
+	static std::string hades_Content()
+	{
+		auto folder  = get_game_executable_folder().parent_path();
+		folder      /= "Content";
+
+		return (char*)folder.u8string().c_str();
+	}
+
+	// Lua API: Function
+	// Table: h2m.paths
+	// Name: Ship
+	// Returns: string: Returns the GameFolder/Ship folder path
+	static std::string hades_Ship()
+	{
+		return (char*)get_game_executable_folder().u8string().c_str();
+	}
+
 	void bind(sol::table& state)
 	{
 		auto ns            = state.create_named("paths");
 		ns["config"]       = config;
 		ns["plugins_data"] = plugins_data;
+		ns["Content"]      = hades_Content;
+		ns["Ship"]         = hades_Ship;
 	}
 } // namespace lua::paths
