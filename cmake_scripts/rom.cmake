2748cd645fdfadcd9cc0bb9ca0e1320afc6ee2e3
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-05-09 16:14:45 +0200

    imgui: fix asserts not getting disabled properly

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -2,6 +2,10 @@ include(FetchContent)
 
 #set(LUA_GIT_HASH a2e0125df529894f5e25d7d477b2df4e37690e0f)
 
+add_compile_definitions(
+    "IMGUI_USER_CONFIG=\"${SRC_DIR}/gui/imgui_config.hpp\""
+)
+
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
