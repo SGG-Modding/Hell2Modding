7b43c121ff06690a88de86a7e43f82c16e041626
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-14 18:22:28 +0200

    lua: plugins have a custom env: see code for details

--- src/hades2/hades_lua.hpp
+++ src/hades2/hades_lua.hpp
@@ -19,14 +19,50 @@ namespace big::hades::lua
 
 		std::scoped_lock l(lua_manager_extension::g_manager_mutex);
 
-		lua_manager_extension::g_lua_manager_instance = std::make_unique<lua_manager>(L,
-		                                                                              g_file_manager.get_project_folder("config"),
-		                                                                              g_file_manager.get_project_folder("plugins_data"),
-		                                                                              g_file_manager.get_project_folder("plugins"),
-		                                                                              [](sol::state_view& state, sol::table& lua_ext)
-		                                                                              {
-			                                                                              lua_manager_extension::init_lua_manager(state, lua_ext);
-		                                                                              });
+		lua_manager_extension::g_lua_manager_instance = std::make_unique<lua_manager>(
+		    L,
+		    g_file_manager.get_project_folder("config"),
+		    g_file_manager.get_project_folder("plugins_data"),
+		    g_file_manager.get_project_folder("plugins"),
+		    [](sol::state_view& state, sol::table& lua_ext)
+		    {
+			    lua_manager_extension::init_lua_manager(state, lua_ext);
+		    },
+		    [](sol::state_view& state) -> sol::environment
+		    {
+			    // rom.game = _G
+			    state[rom::g_lua_api_namespace]["game"] = state["_G"];
+
+			    // local plugin_G = {}
+			    sol::table plugin_G = state.create_table();
+
+			    sol::table all_g = state["_G"];
+			    for (const auto& [k, v] : all_g)
+			    {
+				    if (k.is<const char*>())
+				    {
+					    auto key_str = k.as<const char*>();
+					    // Bad heuristic for filtering out native functions from the game code
+					    if (!std::isupper(static_cast<unsigned char>(key_str[0])))
+					    {
+						    plugin_G[k] = v;
+					    }
+				    }
+				    else
+				    {
+					    plugin_G[k] = v;
+				    }
+			    }
+
+			    // plugin_G.rom = rom
+			    plugin_G[rom::g_lua_api_namespace] = state[rom::g_lua_api_namespace];
+
+			    // plugin_G._G = plugin_G
+			    plugin_G["_G"] = plugin_G;
+
+			    // when you give plugins an _ENV, plugin_G is the __index instead
+			    return sol::environment(state, sol::create, plugin_G);
+		    });
 
 		lua_manager_extension::g_lua_manager_instance->init<lua_module_ext>();
 
--- src/lua_extensions/lua_module_ext.hpp
+++ src/lua_extensions/lua_module_ext.hpp
@@ -26,6 +26,11 @@ namespace big
 	public:
 		lua_module_data_ext m_data_ext;
 
+		lua_module_ext(const module_info& module_info, sol::environment& env) :
+		    lua_module(module_info, env)
+		{
+		}
+
 		lua_module_ext(const module_info& module_info, sol::state_view& state) :
 		    lua_module(module_info, state)
 		{
