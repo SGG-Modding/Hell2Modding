6716bdaabe0fbb25542447f143ac5499ca3237c0
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2024-05-27 01:57:36 +0200

    file_watcher fixes

--- cmake_scripts/rom.cmake
+++ cmake_scripts/rom.cmake
@@ -9,6 +9,6 @@ add_compile_definitions(
 FetchContent_Declare(
 	rom
 	GIT_REPOSITORY https://github.com/xiaoxiao921/ReturnOfModdingBase.git
-	GIT_TAG 6d4fbb379d03a40bbbbf7244293f975d0c72de0d
+	GIT_TAG 2b7f933f9a186545b5accabd3d5350abec14a1b1
 )
 FetchContent_MakeAvailable(rom)
