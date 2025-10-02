76e970b93e06d488af333fa672d9b4377d5b49e5
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2025-10-02 18:18:55 +0200

    fix(cmake): better lovely lib lookup for local dev

--- cmake_scripts/lovely.cmake
+++ cmake_scripts/lovely.cmake
@@ -1,15 +1,21 @@
 include(FetchContent)
 
-# Path to the Rust staticlib
-set(LOVELY_LIB_DIR "${CMAKE_SOURCE_DIR}/lovely-lib/target/x86_64-pc-windows-msvc/release")
+set(LOVELY_LIB_PATHS
+    "${CMAKE_SOURCE_DIR}/lovely-lib/target/x86_64-pc-windows-msvc/release/lovely_lib.lib"
+    "C:/Users/Quentin/source/repos/lovely-lib/target/release/lovely_lib.lib"
+)
 
-find_library(LOVELY_LIB lovely_lib PATHS ${LOVELY_LIB_DIR} NO_DEFAULT_PATH)
+set(LOVELY_LIB "")
 
-if (NOT LOVELY_LIB)
-    set(LOVELY_LIB_DIR "C:/Users/Quentin/source/repos/lovely-lib")
-    find_library(LOVELY_LIB lovely_lib PATHS ${LOVELY_LIB_DIR} NO_DEFAULT_PATH)
-endif()
+foreach(lib_path IN LISTS LOVELY_LIB_PATHS)
+    message(STATUS "Checking for lovely_lib at ${lib_path}")
+    if(EXISTS "${lib_path}")
+        set(LOVELY_LIB "${lib_path}")
+        message(STATUS "Found lovely_lib at ${lib_path}")
+        break()
+    endif()
+endforeach()
 
-if (NOT LOVELY_LIB)
-    message(FATAL_ERROR "Could not find lovely_lib static library")
+if(NOT LOVELY_LIB)
+    message(FATAL_ERROR "Could not find lovely_lib static library in any of the expected locations")
 endif()
