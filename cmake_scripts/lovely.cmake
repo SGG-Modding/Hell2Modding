e1191123e78d22406dc078fd3a5be7bf1d96b9f6
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2025-10-02 15:45:28 +0200

    cmake lovely: add custom path

--- cmake_scripts/lovely.cmake
+++ cmake_scripts/lovely.cmake
@@ -1,9 +1,14 @@
 include(FetchContent)
 
-# Path to Rust staticlib
+# Path to the Rust staticlib
 set(LOVELY_LIB_DIR "${CMAKE_SOURCE_DIR}/target/x86_64-pc-windows-msvc/release")
 
 find_library(LOVELY_LIB lovely_lib PATHS ${LOVELY_LIB_DIR} NO_DEFAULT_PATH)
+
+if (NOT LOVELY_LIB)
+    set(LOVELY_LIB_DIR "C:/Users/Quentin/source/repos/lovely-lib")
+    find_library(LOVELY_LIB lovely_lib PATHS ${LOVELY_LIB_DIR} NO_DEFAULT_PATH)
+endif()
 
 if (NOT LOVELY_LIB)
     message(FATAL_ERROR "Could not find lovely_lib static library")
