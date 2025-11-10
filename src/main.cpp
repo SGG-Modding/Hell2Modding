c63c52b60ceb033b60d66c97b4e11ce23ce73a4e
Quentin <837334+xiaoxiao921@users.noreply.github.com>
2025-11-10 02:23:43 +0100

    safety check Obstacles not having a valid pAnim, happen when porting some Light h1 assets to h2

--- src/main.cpp
+++ src/main.cpp
@@ -1448,36 +1448,24 @@ static void hook_ParseLuaErrorMessageAndAssert(lua_State *msg, const char *len, 
 
 struct sgg_Thing
 {
-	char pad_0000[0x4C];
-	sgg::HashGuid mName; // 0x4C;
+	char pad_1[0x4C];
+	sgg::HashGuid mName;
+
+	char pad_2[192];
+	void *pAnim;
 };
 
-static_assert(offsetof(sgg_Thing, mName) == 0x4C, "sgg::Thing->mName has wrong size.");
-
-
-static sgg_Thing* g_current_sgg_obstacle;
+static_assert(offsetof(sgg_Thing, mName) == 0x4C, "sgg::Thing->mName has wrong offset.");
+static_assert(offsetof(sgg_Thing, pAnim) == 0x1'10, "sgg::Thing->pAnim has wrong offset.");
 
 static bool hook_sgg_Obstacle_IsObscuring(sgg_Thing *this_, uintptr_t thing)
 {
-	g_current_sgg_obstacle = this_;
-
-	return big::g_hooking->get_original<hook_sgg_Obstacle_IsObscuring>()(this_, thing);
-}
-
-static void hook_sgg_Animation_GetParallaxAmount(uintptr_t this_)
-{
-	if (!this_)
+	if (!this_->pAnim)
 	{
-		LOG(ERROR) << "hook_sgg_Animation_GetParallaxAmount: this is null, Obstacle is: "
-		           << g_current_sgg_obstacle->mName.mId << ": "<< lua::hades::data::get_string_from_hash_guid(
-		                  g_current_sgg_obstacle->mName.mId);
-
-		Logger::FlushQueue();
-
-		return;
+		return false;
 	}
 
-	big::g_hooking->get_original<hook_sgg_Animation_GetParallaxAmount>()(this_);
+	return big::g_hooking->get_original<hook_sgg_Obstacle_IsObscuring>()(this_, thing);
 }
 
 static eastl::string *hook_ReadCSString(eastl::string *result, void *file_stream_input)
@@ -2174,18 +2162,6 @@ BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, PVOID)
 				static auto ptr_func = ptr;
 
 				static auto hook_ = hooking::detour_hook_helper::add_queue<hook_sgg_Obstacle_IsObscuring>("", ptr_func);			
-			}
-		}
-
-		{
-			static auto ptr = big::hades2_symbol_to_address["sgg::Animation::GetParallaxAmount"];
-			if (ptr)
-			{
-				static auto ptr_func = ptr;
-
-				static auto hook_ = hooking::detour_hook_helper::add_queue<hook_sgg_Animation_GetParallaxAmount>(
-				    "",
-				    ptr_func);
 			}
 		}
 
